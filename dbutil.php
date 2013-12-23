9f08c616f4933cae72f89c9601a70ca32ac4e674
John Pedroza <mr.john.pedroza@gmail.com>
2013-12-22 21:53:39 -0700

    problem with the query string

--- dbutil.php
+++ dbutil.php
@@ -1,18 +1,17 @@
 <?php
+
+//provides connection to the database for insert into the db
 $connection=mysqli_connect("localhost","p5user","p5user","p4_dontforgetyourcrayons_org");
 // Check connection
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} else {
-	echo "All connected";
-}
+} 
 
-mysqli_query($connection,"INSERT INTO `p4_dontforgetyourcrayons_org`.`activity` (`primaryKey`, `dateAndTime`, `speed`, `miles`, `minutes`, `seconds`) VALUES (NULL, CURRENT_TIMESTAMP, '10', '20', '30', '40');");
-echo "I don't know";
+//$sts = $_POST[speed].toString();
+mysqli_query($connection,"INSERT INTO `p4_dontforgetyourcrayons_org`.`activity` (`primaryKey`, `dateAndTime`, speed, `miles`, `minutes`, `seconds`) 
+VALUES (NULL, CURRENT_TIMESTAMP, $_POST[speed], '12', '58', '27')");
 
 
-//INSERT INTO `p4_dontforgetyourcrayons_org`.`activity` (`primaryKey`, `dateAndTime`, `speed`, `miles`, `minutes`, `seconds`) VALUES (NULL, CURRENT_TIMESTAMP, '1', '2', '3', '4');
-// SELECT * FROM `activity` 
 mysqli_close($connection);
 
 ?>
--- js/util.js
+++ js/util.js
@@ -1,5 +1,12 @@
+
+//provides the time at the very top of the screen
 function myFunction() {
 	var d = new Date();
 	var x = document.getElementById("top");
 	x.innerHTML=d.toDateString();
+}
+
+//used to ensure only numbers are passed - gives light protection from sql injection attacks using regular expressions
+function numericFilter(txb) {
+   txb.value = txb.value.replace(/[^\0-9]/ig, "");
 }
